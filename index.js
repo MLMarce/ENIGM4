40ae56564234927e527e8f3b6c157d1d94d8405d
Marcelo Lencina <marcelodaniellencina@gmail.com>
2023-08-11 12:16:16 -0300

    modificacion a data.json y maquetado de vista del producto

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ const menuMobile = document.querySelector('.menu-mobile');
 const headerMobile = document.querySelector('.header-mobile');
 const inputSearch = document.querySelector('.input-search');
 const btnSearch = document.querySelector('.btn-search');
-const form = document.querySelector('.form');
 const principalSection = document.querySelector('.principal-section');
 const menuCategories = document.querySelector('#categories');
 
 // listeners del menu lateral en mobile
 barsIcon.addEventListener('click', abrirAside);
 xIcon.addEventListener('click', cerrarAside);
-// btnSearch.addEventListener('click', buscarCategoria);
+btnSearch.addEventListener('click', buscar);
 menuCategories.addEventListener('click', crearCategorias);
 menuCategories.addEventListener('click', cerrarAside);
 
@@ -29,16 +28,30 @@ function abrirAside() {
     headerMobile.classList.add('inactive');
 }
 
-// function buscarCategoria() { //renombrar y mejorar para que se pueda buscar cualquier producto.
-//     let dir = inputSearch.value;
-//     if(inputSearch.value == "inicio") {
-//         dir = "index";
-//     }
+function buscar() { //renombrar y mejorar para que se pueda buscar cualquier producto.
+    let valorInput = inputSearch.value;
+    console.log(valorInput);
+    let datos = data;
+    let categoria;
+    let productos = [];
 
-//     form.setAttribute('action', `/${dir}.html`);
+    //obteniendo lista de categorias y cantidad de productos
+    for(let dato of datos) {
+        categoria = dato.categoryName;
+        productos = dato.productos;
+        console.log(productos);
+        if(categoria.includes(valorInput)) {
+            crearProductos();//crear esta funcion para renderizar los productos de dicha categoria
+            cerrarAside();
+        } // else if para en el caso de que se especifique un producto mostras solo ese producto. y ocultar el aside.
+    }
+
+
     
-//     principalSection.classList.remove('inactive');
-// }
+
+    
+    principalSection.classList.remove('inactive');
+}
 
 //renderizando dinamicamente las categorias en la seccion principal
 function crearCategorias() {
@@ -51,9 +64,7 @@ function crearCategorias() {
             <p class="categories__name">${dato.categoryName}</p>
         </div>
         `;
-        console.log(dato.img);
-        console.log(dato.categoryName);
     }
-}
 
-crearCategorias();
+
+}
